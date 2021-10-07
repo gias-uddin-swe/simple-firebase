9fdbcc7c4f4fdb38f1549dc1d45eefbb62dd91f7
gias-uddin-swe <nirobh753@gmail.com>
2021-10-07 19:16:59 +0600

    first commit

--- src/Firebase/Firebase.config.js
+++ src/Firebase/Firebase.config.js
@@ -1,10 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBhoMzn3VfgW4S5tkY6vzMKtyYzTa4CHMo",
-  authDomain: "fireabse-authentication.firebaseapp.com",
-  projectId: "fireabse-authentication",
-  storageBucket: "fireabse-authentication.appspot.com",
-  messagingSenderId: "1036586571476",
-  appId: "1:1036586571476:web:c5deb04b96c71f07134d41",
+ //put your won config
 };
 
 export default firebaseConfig;
